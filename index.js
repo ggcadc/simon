afd0eef27dafe80631384ba9137f274ed30aa921
Grant Glidewell <ggcadc@gmail.com>
2017-08-11 21:13:49 -0700

    nothing fixed

--- index.js
+++ index.js
@@ -17,41 +17,33 @@ const redAct = () => {
   redSound.currentTime = 0;
   redSound.play();
   document.getElementById('red').style = 'background-color: rgb(249, 128, 128)';
-  setTimeout(() => {document.getElementById('red').style = 'background-color: rgb(195, 45, 45)'}, 250);
+  setTimeout(() => {document.getElementById('red').style = 'background-color: rgb(195, 45, 45)'}, 100);
   displayCount.innerHTML = count;
-  console.log('red');
-  return;
 }
 const blueAct = () =>{
   blueSound.currentTime = 0;
   blueSound.play();
   document.getElementById('blue').style = 'background-color: rgb(156, 165, 255)';
-  setTimeout(() => {document.getElementById('blue').style = 'background-color: rgb(75, 98, 195)'}, 250);
+  setTimeout(() => {document.getElementById('blue').style = 'background-color: rgb(75, 98, 195)'}, 100);
   displayCount.innerHTML = count;
-  console.log('blue');
-  return;
 }
 const greenAct = () =>{
   greenSound.currentTime = 0;
   greenSound.play();
   document.getElementById('green').style = 'background-color: rgb(130, 249, 128)';
-  setTimeout(() => {document.getElementById('green').style = 'background-color: rgb(45, 195, 45)'}, 250);
+  setTimeout(() => {document.getElementById('green').style = 'background-color: rgb(45, 195, 45)'}, 100);
   displayCount.innerHTML = count;
-  console.log('green');
-  return;
 }
 const yellowAct = () =>{
   yellowSound.currentTime = 0;
   yellowSound.play();
   document.getElementById('yellow').style = 'background-color: rgb(236, 255, 172)';
-  setTimeout(() => {document.getElementById('yellow').style = 'background-color: rgb(182, 214, 55)'}, 250);
+  setTimeout(() => {document.getElementById('yellow').style = 'background-color: rgb(182, 214, 55)'}, 100);
   displayCount.innerHTML = count;
-  console.log('yellow');
-  return;
 }
 
 const strictMode = () => {
-  if(strict === false){
+  if(!strict){
     strict = true;
     strictButton.style = 'background-color: rgb(255, 244, 185); color: red';
     return;
@@ -62,53 +54,52 @@ const strictMode = () => {
 
 let gameArr = [];
 let count = 1;
-let move = 20;
+let move = 7;
 
-const startGame = () => {
-  count = 1;
+const createGameArr = () => {
   for(let i = 0; i < 20; i++){
     let rnd = Math.floor(Math.random() * 5);
     gameArr.splice(i, 0, rnd);
-  }
+    }
+}
+
+const startGame = () => {
+  createGameArr();
+  count = 1;
   turnInit();
-  return;
 }
 
 const turnInit = () => {
   if(move > 0){
-    clearTimeout();
-    compMove();
-    return;
+    setTimeout(() => compMove(), 500);
   }else{
     copyMove();
-    return;
  }
 }
 
 const compMove = () => {
-  move = move - 1;
   if(gameArr[move] === 1){
     redAct();
-    setTimeout(() => turnInit(), 400);
-    return;
+    move = move - 1;
+    turnInit();
 
   }
   if(gameArr[move] === 2){
     blueAct();
-    setTimeout(() => turnInit(), 400);
-    return;
+    move = move - 1;
+    turnInit();
 
   }
   if(gameArr[move] === 3){
     yellowAct();
-    setTimeout(() => turnInit(), 400);
-    return;
+    move = move - 1;
+    turnInit();
 
   }
   if(gameArr[move] === 4){
     greenAct();
-    setTimeout(() => turnInit(), 400);
-    return;
+    move = move - 1;
+    turnInit();
 
   }
 
@@ -116,7 +107,6 @@ const compMove = () => {
 
 const copyMove = () => {
   console.log('human move')
-  return;
 }
 
 window.redSquare.addEventListener('click', () => redAct());
